5db350294c9b9cd7088e304937f356d180f843d6
Daniel Naylor <git@drnaylor.co.uk>
2021-03-23 19:53:23 +0000

    Add TwoDimensionalPortal for teleport events

--- src/main/java/org/spongepowered/api/event/EventContextKeys.java
+++ src/main/java/org/spongepowered/api/event/EventContextKeys.java
@@ -50,6 +50,7 @@ import org.spongepowered.api.world.LocatableBlock;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.explosion.Explosion;
+import org.spongepowered.api.world.portal.Portal;
 import org.spongepowered.api.world.server.ServerLocation;
 import org.spongepowered.api.world.server.ServerWorld;
 import org.spongepowered.math.vector.Vector3d;
@@ -232,6 +233,11 @@ public final class EventContextKeys {
      * Represents a {@link PluginContainer}.
      */
     public static final EventContextKey<PluginContainer> PLUGIN = EventContextKeys.key(ResourceKey.sponge("plugin"), TypeTokens.PLUGIN_CONTAINER_TOKEN);
+
+    /**
+     * Represents a {@link Portal}.
+     */
+    public static final EventContextKey<Portal> PORTAL = EventContextKeys.key(ResourceKey.sponge("portal"), TypeTokens.PORTAL_TYPE_TOKEN);
 
     /**
      * Represents a {@link ProjectileSource}.
